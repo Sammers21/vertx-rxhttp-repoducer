dfe0cdca22cfd74be0dcafe1794c7fd3b4867add
Sammers21 <titantins@gmail.com>
2020-02-12 19:05:58 +0300

    use vertx junit 5

--- src/test/java/io/vertx/starter/HttpTest.java
+++ src/test/java/io/vertx/starter/HttpTest.java
@@ -1,21 +1,27 @@
 package io.vertx.starter;
 
 import io.reactivex.Flowable;
+import io.vertx.junit5.Checkpoint;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.core.buffer.Buffer;
 import io.vertx.reactivex.ext.web.client.WebClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 
+@ExtendWith(VertxExtension.class)
 public class HttpTest {
 
   @Test
-  public void httpRequestFlow() throws IOException {
+  public void httpRequestFlow(VertxTestContext ctx) throws IOException {
     Vertx vertx = Vertx.vertx();
     final int port = rndPort();
+    final Checkpoint responseReceived = ctx.checkpoint(1);
     final String str = "123";
     vertx.createHttpServer().requestHandler(request -> {
       Flowable.fromArray(
@@ -23,16 +29,17 @@ public class HttpTest {
       ).subscribe(request.response().toSubscriber());
     }).rxListen(port)
       .blockingGet();
-    final String resp = WebClient.create(vertx).get(port, "127.0.0.1", "/hello")
-      .rxSend()
-      .blockingGet()
-      .bodyAsString();
-    Assertions.assertEquals(str, resp);
-    vertx.close();
+    WebClient.create(vertx).get(port, "127.0.0.1", "/hello")
+      .send(ctx.succeeding(event -> {
+        String response = event.bodyAsString();
+        Assertions.assertEquals(str, response);
+        responseReceived.flag();
+      }));
   }
 
   /**
    * Find a random port.
+   *
    * @return The free port.
    * @throws IOException If fails.
    */
